8a163ac8a6406c49a3f068a0752affb6e15fed99
dpulenko <dpulenko@gbsfo.com>
2020-01-30 15:01:37 +0200

    Sending Http Requests

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,10 +8,19 @@ const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [
-      ...prevIngredients, 
-      { id: Math.random().toString(), ...ingredient }
-    ]);
+    fetch('https://react-hooks-df7fd.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {'Content-Type': 'application/json'}
+    })
+      .then(res => {
+        return res.json();
+      }).then(resData => {
+        setUserIngredients(prevIngredients => [
+          ...prevIngredients, 
+          { id: resData.name, ...ingredient }
+        ]);
+      });
   };
 
   return (
